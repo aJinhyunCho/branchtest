0cdf9c89361243b8814e2387ac997e19c85ec268
Jinhyun Cho <jinhyun.cho@access-company.com>
2014-01-08 14:19:17 +0900

    ssss

--- test.rb
+++ test.rb
@@ -2,7 +2,7 @@ test.rb sjlfjsjsl
 added by testbranch
 this is modified from testing branch
 test test test
-new test
+new test new message from master
 modified form testing branch
 new branch
 new branch sfsfafsdaf
